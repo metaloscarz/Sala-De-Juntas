f98b41cbc0dec5f7cab32685da411ec9917a5dfa
Oscar Manuel <oscar-lez@hotmail.com>
2021-01-31 19:30:00 -0600

    adding routes

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,19 @@
 | It is a breeze. Simply tell Lumen the URIs it should respond to
 | and give it the Closure to call when that URI is requested.
 |
-*/
+ */
 
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+	return $router->app->version();
+});
+
+$router->post('login', 'LoginController@login');
+$router->post('register', 'RegisterController@register');
+
+// aplicamos el middleware auth
+$router->group(['middleware' => 'auth'], function () use ($router) {
+
+	// aqui van todas las rutas que se necesitar estar autenticado para el acceso
+	$router->post('logout', 'LoginController@logout');
+
 });
