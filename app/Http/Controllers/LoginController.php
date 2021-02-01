4d30e40b4303064604e2585c5805a832104eaf9c
Oscar Manuel <oscar-lez@hotmail.com>
2021-01-31 19:16:18 -0600

    adding Controller for login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+
+class LoginController extends Controller {
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function login(Request $req) {
+		// validacion del request
+		$this->validate($req, [
+			'email' => 'email|required',
+			'password' => 'required',
+		]);
+
+		// consulta del usuario
+		$user = User::query()->where('email', $req->input('email'))->first();
+
+		// verificaion de contraseña
+		if (Crypt::decrypt($user->password) === $req->input('password')) {
+
+			// creamos un string random para el token
+			$api_token = str_random(50);
+
+			// le seteamos el api_token al usuario
+			$user->api_token = $api_token;
+
+			// guardamos
+			$user->save();
+
+			// retornamos el api_token, para futuras peticiones
+			return response()->json(['status' => 'success', 'api_token' => $api_token]);
+		} else {
+			return response()->json(['status' => 'fail'], 401);
+		}
+
+	}
+
+	public function logout(Request $req) {
+		User::where('api_token', $req->input('api_token'))->update(['api_token' => null]);
+		return response()->json(['status' => 'success']);
+	}
+}
