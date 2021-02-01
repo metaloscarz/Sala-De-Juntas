760016ba3918da54d2662824320980abac7d7f09
Oscar Manuel <oscar-lez@hotmail.com>
2021-01-31 19:12:41 -0600

    adding migration and model for User

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,25 +9,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
-{
-    use Authenticatable, Authorizable, HasFactory;
+class User extends Model implements AuthenticatableContract, AuthorizableContract {
+	use Authenticatable, Authorizable, HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email',
-    ];
-
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-    ];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'name', 'email', 'role', 'password', 'create_at', 'update_at', 'api_token',
+	];
 }
