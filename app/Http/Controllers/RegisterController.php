8ce767bd8b1b52c9f682cab5ff660dcb3f588c74
Oscar Manuel <oscar-lez@hotmail.com>
2021-01-31 19:18:19 -0600

    adding Controller for register

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+
+class registerController extends Controller {
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function register(Request $req) {
+		$data = $req->all();
+		// validamos los datos
+		$this->validate($req, [
+			'name' => 'required',
+			'email' => 'email|required',
+			'password' => 'required',
+		]);
+
+		// ciframos el password
+		$data['password'] = Crypt::encrypt($data['password']);
+
+		// creamos un nuevo usuario
+		$user = User::create($data);
+		return response()->json(['status' => 'success', 'data' => $user]);
+	}
+}
